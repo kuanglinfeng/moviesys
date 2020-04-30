05101d673b789d3e14188726757b63c8ce537d91
Flinn <358530466@qq.com>
2020-04-30 18:03:21 +0800

    start admin

---
+++ admin/src/services/CommonTypes.ts
@@ -0,0 +1,21 @@
+export interface IResponseError {
+  error: string
+  data: null
+}
+
+export interface IResponseData<T> {
+  error: ''
+  data: T
+}
+
+export interface IResponsePageData<T> {
+  error: ''
+  total: number
+  data: T[]
+}
+
+export interface ISearchCondition {
+  page?: number
+  limit?: number
+  key?: string
+}
