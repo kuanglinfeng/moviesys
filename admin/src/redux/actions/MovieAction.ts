4d45588e66cddaeacf9df7cace24c95b7dd1578d
Flinn <358530466@qq.com>
2020-05-14 20:22:17 +0800

    初步完成reducer的功能

--- admin/src/redux/actions/MovieAction.ts
+++ admin/src/redux/actions/MovieAction.ts
@@ -1,18 +1,93 @@
 import { IMovie } from '../../services/MovieService'
 import { IAction } from './ActionTypes'
+import { ISearchCondition, SwitchType } from '../../services/CommonTypes'
 
+/**
+ * 保存电影的action的类型注解
+ */
 export type SaveMoviesAction = IAction<'movie_save', {
   movies: IMovie[]
   total: number
 }>
 
-function saveMovieAction(movies: IMovie[], total: number): SaveMoviesAction {
+/**
+ * 保存电影的action的创建函数
+ * @param movies
+ * @param total
+ */
+const saveMovieAction = (movies: IMovie[], total: number): SaveMoviesAction => ({
+  type: 'movie_save',
+  payload: {
+    movies,
+    total
+  }
+})
+
+/**
+ * 设置Loading状态的action的类型注解
+ */
+export type SetLoadingAction = IAction<'movie_setLoading', boolean>
+
+/**
+ * 设置Loading状态的action的创建函数
+ * @param isLoading
+ */
+const setLoadingAction = (isLoading: boolean): SetLoadingAction => ({
+  type: 'movie_setLoading',
+  payload: isLoading
+})
+
+/**
+ * 设置电影查询条件的action的类型注解
+ */
+export type SetConditionAction = IAction<'movie_setCondition', ISearchCondition>
+
+/**
+ * 设置查询电影条件的action的创建函数
+ * @param condition
+ */
+const setConditionAction = (condition: ISearchCondition): SetConditionAction => ({
+  type: 'movie_setCondition',
+  payload: condition
+})
+
+/**
+ * 删除电影的action的类型注解
+ */
+export type DeleteAction = IAction<'movie_delete', string>
+
+/**
+ * 创建删除电影的action的创建函数
+ * @param id
+ */
+const deleteAction = (id: string): DeleteAction => ({
+  type: 'movie_delete',
+  payload: id
+})
+
+export type MovieChangeSwitchAction = IAction<'movie_switch', {
+  type: SwitchType,
+  newVal: boolean,
+  id: string
+}>
+
+function changeSwitchAction(type: SwitchType, newVal: boolean, id: string): MovieChangeSwitchAction {
   return {
-    type: 'movie_save',
+    type: 'movie_switch',
     payload: {
-      // 负载、负荷
-      movies,
-      total
+      type,
+      newVal,
+      id
     }
   }
+}
+
+// 所有的Action情况
+export type MovieActions = SaveMoviesAction | SetLoadingAction | SetConditionAction | DeleteAction
+
+export default {
+  saveMovieAction,
+  setLoadingAction,
+  setConditionAction,
+  deleteAction
 }
--- admin/src/redux/reducers/MovieReducer.ts
+++ admin/src/redux/reducers/MovieReducer.ts
@@ -2,6 +2,14 @@
 
 import { IMovie } from '../../services/MovieService'
 import { ISearchCondition } from '../../services/CommonTypes'
+import {
+  DeleteAction,
+  MovieActions,
+  SaveMoviesAction,
+  SetConditionAction,
+  SetLoadingAction
+} from '../actions/MovieAction'
+import { Reducer } from 'react'
 
 // 让ISearchCondition接口里的类型都变为必填的
 export type IMovieCondition = Required<ISearchCondition>
@@ -23,4 +31,58 @@ export interface IMovieState {
    * 是否正在加载数据
    */
   isLoading: boolean
+}
+
+const defaultState: IMovieState = {
+  data: [],
+  condition: {
+    page: 1,
+    limit: 10,
+    key: ''
+  },
+  total: 0,
+  isLoading: false
+}
+
+type MovieReducer<A> = Reducer<IMovieState, A>
+
+const saveMovie: MovieReducer<SaveMoviesAction> = (state, action) => ({
+  ...state,
+  data: action.payload.movies,
+  total: action.payload.total
+})
+
+const setCondition: MovieReducer<SetConditionAction> = (state, action) => ({
+  ...state,
+  condition: {
+    ...state.condition,
+    ...action.payload
+  }
+})
+
+const setLoading: MovieReducer<SetLoadingAction> = (state, action) => ({
+  ...state,
+  isLoading: action.payload
+})
+
+const deleteMovie: MovieReducer<DeleteAction> = (state, action) => ({
+  ...state,
+  data: state.data.filter(movie => movie._id !== action.payload),
+  total: state.total - 1
+})
+
+
+export default (state: IMovieState = defaultState, action: MovieActions): IMovieState => {
+  switch (action.type) {
+    case 'movie_save':
+      return saveMovie(state, action)
+    case 'movie_setLoading':
+      return setLoading(state, action)
+    case 'movie_setCondition':
+      return setCondition(state, action)
+    case 'movie_delete':
+      return deleteMovie(state, action)
+    default:
+      return state
+  }
 }
