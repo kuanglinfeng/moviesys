1d036c6472a9229286beef4200b48062b9fdc11a
Flinn <358530466@qq.com>
2020-05-16 18:23:17 +0800

    完成了Layout组件

--- admin/src/pages/Layout.tsx
+++ admin/src/pages/Layout.tsx
@@ -4,34 +4,43 @@ import Home from './Home'
 import MovieList from './movie/MovieList'
 import AddMovie from './movie/AddMovie'
 import EditMovie from './movie/EditMovie'
+import { Layout, Menu } from 'antd'
 
-const Layout: React.FC = () => {
+const { Header, Sider, Content } = Layout
+
+const _Layout: React.FC = () => {
   return (
-    <div>
-      <header>
-        <ul>
-          <li>
-            <NavLink to='/'>首页</NavLink>
-          </li>
-          <li>
-            <NavLink to='/movie'>电影</NavLink>
-          </li>
-          <li>
-            <NavLink to='/movie/add'>添加电影</NavLink>
-          </li>
-          <li>
-            <NavLink to='/movie/edit/123df12'>修改电影</NavLink>
-          </li>
-        </ul>
-        <div>
-          <Route path='/' component={ Home } exact />
-          <Route path='/movie' component={ MovieList } exact />
-          <Route path='/movie/add' component={ AddMovie } exact />
-          <Route path='/movie/edit/:id' component={ EditMovie } exact />
-        </div>
-      </header>
+    <div className='container'>
+        <Layout>
+          <Header className='header'>
+            <NavLink to='/'>猫眼电影管理系统</NavLink>
+          </Header>
+          <Layout>
+            <Sider>
+              <Menu
+                mode="inline"
+                theme="dark"
+              >
+                <Menu.Item key="1" >
+                  <NavLink to='/movie'>电影列表</NavLink>
+                </Menu.Item>
+                <Menu.Item key="2" >
+                  <NavLink to='/movie/add'>添加电影</NavLink>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Content>
+              <div className='main'>
+                <Route path='/' component={ Home } exact />
+                <Route path='/movie' component={ MovieList } exact />
+                <Route path='/movie/add' component={ AddMovie } exact />
+                <Route path='/movie/edit/:id' component={ EditMovie } exact />
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
     </div>
   )
 }
 
-export default Layout
+export default _Layout
