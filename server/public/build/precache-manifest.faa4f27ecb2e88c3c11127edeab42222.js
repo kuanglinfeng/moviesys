6ada8de553b83740f81328c7c617d7cb0a2c916d
Flinn <358530466@qq.com>
2020-06-03 20:56:43 +0800

    完成

---
+++ server/public/build/precache-manifest.faa4f27ecb2e88c3c11127edeab42222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea992451d58ce3b8ddb75100b153ecf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ef4548aacd39afcb08",
+    "url": "/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "378f38064051f7f1e966",
+    "url": "/static/css/main.4ab4ee0e.chunk.css"
+  },
+  {
+    "revision": "e0ef4548aacd39afcb08",
+    "url": "/static/js/2.c90cab79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c90cab79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378f38064051f7f1e966",
+    "url": "/static/js/main.9436fb6a.chunk.js"
+  },
+  {
+    "revision": "eac0ebdc7d8871a652be",
+    "url": "/static/js/runtime-main.cb9e32b0.js"
+  }
+]);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata'
 import Express from 'express'
 import MovieRouter from './routes/MovieRoute'
 import UploadRoute from './routes/UploadRoute'
+import history from 'connect-history-api-fallback'
 
 const app = Express()
 
+app.use(history())
+app.use('/', Express.static('public/build'))
 app.use('/upload', Express.static('public/upload'))
 
 // 配置中间件用于解析请求消息体中的json格式数据
